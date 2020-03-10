5ea937ea08a1b6c7d94604a74a22946a62b55004
niksram <nikhilsram.off@gmail.com>
2020-03-11 00:30:40 +0530

    final commit for day-1 yet to test min_heirarchy and tagbit allocation... still working on load.

--- cache_mapper.cpp
+++ cache_mapper.cpp
@@ -14,6 +14,7 @@ private:
     long int set_size;
     long int lru;
     vector<vector<long int>> container;
+    long int min_heirarchy(long int set, long int update);
 
 public:
     cache_mock(long int, long int, long int, long int, long int = 0, long int = 0);
@@ -33,13 +34,15 @@ cache_mock::cache_mock(long int d, long int c, long int m, long int b, long int 
 
 void cache_mock::container_init()
 {
+    if (container.size())
+        container.clear();
     long int size = cache_size / block_size;
     for (long int i = 0; i < size; i++)
     {
         vector<long int> block;
         block.push_back(i);
-        block.push_back(-1); //tag_bit
-        block.push_back(-1); //dirty_bit
+        block.push_back(-1);           //tag_bit
+        block.push_back(-1);           //dirty_bit
         block.push_back(i % set_size); //heirarchy
         for (long int j = 0; j < block_size; j++)
         {
@@ -51,7 +54,7 @@ void cache_mock::container_init()
 
 void cache_mock::container_display()
 {
-    printf("ID\tTag\tDirty\tHeirarchy\t\tBlock-Display\n");
+    printf("ID\tTag\tDirty\tHeirarchy\tBlock-Display\n");
     for (long int i = 0; i < container.size(); i++)
     {
         if (!(i % set_size))
@@ -72,11 +75,44 @@ void cache_mock::container_display()
 
 long int cache_mock::loader(long int address)
 {
+    if (address >= mem_size)
+        return -1;
+    long int block = address / block_size;
+    long int set = block / set_size;
+    long int tag = mem_size / cache_size;
+    for (int i = 4; i < container[0].size(); i++)
+    {
+        container[set * set_size + minh][i] = block * block_size + i - 4;
+    }
+}
+
+long int cache_mock::min_heirarchy(long int set, long int update)
+{
+    long int block = set * set_size;
+    long int min = block;
+    long int max = block;
+    for (long int i = 0; i < set_size; i++)
+    {
+        if (container[set * set_size + i][3] < container[min][3])
+        {
+            min = set * set_size + i;
+        }
+        if (ontainer[set * set_size + i][3] > container[max][3])
+        {
+            max = set * set_size + i;
+        }
+    }
+    if (update)
+    {
+        container[set * set_size + i][3] = container[max][3] + 1;
+    }
+    return min;
 }
 
 int main()
 {
-    cache_mock a(0, 16, 2048, 2, 2);
+    cache_mock a(1, 16, 2048, 2);
+    a.loader(17);
     a.container_display();
     return 0;
 }
