6724eb345bfb2a8bab3e16b5388e7968b5144ea0
niksram <nikhilsram.off@gmail.com>
2020-03-11 22:08:14 +0530

    storer done, seems working fine for fully-associative problem. testing yet to be done

--- cache_mapper.cpp
+++ cache_mapper.cpp
@@ -23,6 +23,7 @@ public:
     void container_init();
     void container_display();
     long int loader(long int);
+    long int storer(long int);
 };
 
 cache_mock::cache_mock(long int d, long int c, long int m, long int b, long int s, long int l) : desc(d), cache_size(c), mem_size(m), block_size(b), set_size(s), lru(l)
@@ -30,7 +31,7 @@ cache_mock::cache_mock(long int d, long int c, long int m, long int b, long int 
     if (d == 1)
         set_size = 1;
     else if (d == 2)
-        set_size = c;
+        set_size = c/b;
     no_sets = cache_size / (block_size * set_size);
     container_init();
 }
@@ -100,7 +101,37 @@ long int cache_mock::loader(long int address)
     for(int i=4;i<container[0].size();i++)
     {
         container[min_heir][i]=block*block_size+i-4;
-    }    
+    }
+    return 0;    
+}
+
+long int cache_mock::storer(long int address)
+{
+    if (address >= mem_size)
+        return -1;
+    long int block = address / block_size;
+    long int set = block % no_sets;
+    long int tag = address / (block_size * no_sets);
+    for (long int i = 0; i < set_size; i++) // Hit Case!!!
+    {
+        if (container[set * set_size + i][1] == tag)
+        {
+            if (lru)
+                container[set * set_size + i][3] = container[find_heirarchy_max(set)][3] + 1;
+            container[set*set_size+i][2]=1;
+            return 1;
+        }
+    }
+    //miss case
+    long int min_heir=find_heirarchy_min(set);
+    container[min_heir][1]=tag;
+    container[min_heir][2]=0;
+    container[min_heir][3]=container[find_heirarchy_max(set)][3]+1;
+    for(int i=4;i<container[0].size();i++)
+    {
+        container[min_heir][i]=block*block_size+i-4;
+    }
+    return 0;    
 }
 
 long int cache_mock::find_heirarchy_max(long int set)
@@ -131,9 +162,9 @@ long int cache_mock::find_heirarchy_min(long int set)
 
 int main()
 {
-    cache_mock a(1, 16, 2048, 2);
+    cache_mock a(2, 16, 2048, 2);
     cout<<a.loader(2047)<<endl;
-    cout<<a.loader(2047)<<endl;
+    cout<<a.loader(1576)<<endl;
     a.container_display();
     return 0;
 }
